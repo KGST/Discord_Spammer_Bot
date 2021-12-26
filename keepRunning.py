2c9e489b2ce0c2f13e4dcb4d01bddcb406815143
KGST <68213725+KGST@users.noreply.github.com>
2021-12-25 17:19:47 -0800

    Update keepRunning.py

--- keepRunning.py
+++ keepRunning.py
@@ -1,3 +1,4 @@
+#KEEP THE BOT RUNNING BY MAKING A WEBSITE AND USING A THIRD PARTY (ie Uptime robot) TO PING IT EVERY ONCE IN A WHILE 
 from flask import Flask
 from threading import Thread
 
@@ -11,5 +12,5 @@ def run():
   app.run(host='0.0.0.0',port=8080)
 
 def keepAlive():
-    t = Thread(target=run)
-    t.start()
+    threadRun = Thread(target=run)
+    threadRun.start()
